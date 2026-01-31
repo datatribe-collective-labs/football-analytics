2257fcd2fe998fc7df65e1ad542b8a22c2250921
fredie7 <52379604+fredie7@users.noreply.github.com>
2026-01-31 12:46:13 +0200

    Create bronze_top_scorers.py

---
+++ bronze/bronze_top_scorers.py
@@ -0,0 +1,45 @@
+# Import dependencies
+from pyspark.sql.functions import explode, col, collect_list, struct, array_distinct, current_timestamp
+
+# Load top scorers' data
+raw_scorers_data = spark.read.format("json").load(
+    "abfss://raw@footballanalyticstorage.dfs.core.windows.net/scorers"
+)
+
+# Show the data
+display(raw_scorers_data)
+
+# Flatten scorers data to obtain each team's scorers information
+scorers_data = (
+    raw_scorers_data
+        .select(explode(col("scorers")).alias("scorer"))
+)
+
+
+# Deconstruct the scorers data
+bronze_top_scorers = (
+    scorers_data
+        .select(
+            col("scorer.team.id").alias("club_id"),
+            col("scorer.team.name").alias("football_club"),
+            col("scorer.playedMatches"),
+            col("scorer.goals"),
+            col("scorer.assists"),
+            col("scorer.penalties"),
+            col("scorer.player.id").alias("player_id"),
+            col("scorer.player.name").alias("player_name"),
+            col("scorer.player.section")
+        )
+        .withColumn("ingestion_ts", current_timestamp())
+)
+
+# Confirm data transformation
+display(bronze_top_scorers)
+
+# Write data to delta lake and save as table
+bronze_top_scorers.write \
+    .format("delta") \
+        .mode("append") \
+            .partitionBy("ingestion_ts") \
+            .option("path","abfss://bronze@footballanalyticstorage.dfs.core.windows.net/top_scorers") \
+                .saveAsTable("db_ws.bronze.top_scorers")
